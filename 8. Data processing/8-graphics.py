2713300bc2486df4baf9f3306872f3e151709f1e
Dmitry Tikhonov <tikhonov.dr@phystech.edu>
2023-05-17 10:20:35 +0300

    edit 8-1 script

--- 8. Data processing/8-graphics.py
+++ 8. Data processing/8-graphics.py
@@ -8,17 +8,13 @@ with open('/home/b01-206/get/8. Data processing/settings.txt') as settings_file:
     settings = [float(i) for i in settings_file.read().split('\n')]
 
 data = np.loadtxt('/home/b01-206/get/8. Data processing/data.txt', dtype = int) * settings[1]
-data_time = np.array([i * settings[0] for i in range(data.size)])
+data_time = np.array([i/settings[0] for i in range(data.size)])
 
 #параметры фигуры
 fig, ax = pyplot.subplots(figsize = (16, 10), dpi = 500)
 
 #устанавливаем минимальные и максимальные значения для осей
 ax.set(xlim = (data_time.min(), data_time.max()), ylim = (data.min(), data.max()))
-
-#устанавливаем интервал основных делений
-# ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-# ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
 
 #название графика
 ax.set_title("\n".join(wrap('Процесс зарядки и разрядки конденсатора в RC-цепи', 60)), loc = 'center')
@@ -33,9 +29,12 @@ ax.set_ylabel("Напряжение U, В")
 ax.set_xlabel("Время t, с")
 
 #линия с легендой
-ax.plot(data_time, data, c = 'black', linewidth = 1, label = 'U(t)')
-ax.scatter(data_time[0:data.size:20], data[0:data.size:20], marker = 's', c = 'blue', s = 10)
-ax.legend(shadow = False, loc = 'right', fontsize = 30)
+ax.plot(data_time, data, c = 'red', linewidth = 2, label = 'График зависимости U(t)')
+ax.scatter(data_time[0:data.size:20], data[0:data.size:20], marker = 'o', c = 'black', s = 10)
+ax.legend(shadow = False, loc = 'right', fontsize = 14)
+
+#время разрядки и зарядки на графике
+ax.text(50.1, 2.25, "Время зарядки = 22.11 с \n" + "Время разрядки = 45.28 с ", fontsize = 14)
 
 #сохранение графика
 fig.savefig('/home/b01-206/get/8. Data processing/graph.png')
